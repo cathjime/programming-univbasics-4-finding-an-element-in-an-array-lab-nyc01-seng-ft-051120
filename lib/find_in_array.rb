1df377b3fa1fe3e2f20a88807a01fcae88ee467e
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-30 15:23:14 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,9 +3,9 @@ def find_element_index(array, value_to_find)
   count = 0 
   
   while count < array.size do
-    if array[count] === value_to_find
-      count 
-    else
-      nil 
+    if array[count] == value_to_find
+      return count 
     end 
+    count += 1 
+  end 
 end
